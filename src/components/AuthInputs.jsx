2f1cbda9ee75df8c5795c782cc1f5fdea5bd95d1
Shlomzion <itayca40@gmail.com>
2023-12-03 20:09:09 +0200

    put a dynamic data inside the styled

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -16,15 +16,16 @@ font-size: 0.75rem;
 font-weight: 700;
 letter-spacing: 0.1em;
 text-transform: uppercase;
-color: #6b7280;
+color: ${({$invalid}) => $invalid? '#f87171' : '#6b7280' }
+
 `
 const Input = styled.input`
 width: 100%;
 padding: 0.75rem 1rem;
 line-height: 1.5;
-background-color: #d1d5db;
-color: #374151;
-border: 1px solid transparent;
+background-color: ${({$invalid}) => $invalid?'#fed2d2' : '#d1d5db'};
+color: ${({$invalid}) => $invalid?'#ef4444' : '#374151'};
+border:  1px solid ${({$invalid}) => $invalid?'#f73f3f' : 'transparent'};
 border-radius: 0.25rem;
 box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -52,18 +53,19 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <Label className = {`label ${emailNotValid ? 'invalid' : ''}`}>Email</Label>
+          {/* because inValid is a built- in prop we will add a $ sign so we wont get an error */}
+          <Label $invalid = {emailNotValid}>Email</Label> 
           <Input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            $invalid = {emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <Label className = {`label ${passwordNotValid ? 'invalid' : ''}`}>Password</Label>
+          <Label  $invalid = {passwordNotValid}>Password</Label>
           <Input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            $invalid ={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
